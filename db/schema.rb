4b7bf5ba50cffa6691bccf64fc99240315c43fd7
Sebastian McKee <sebmcke@gmail.com>
2025-03-06 17:13:10 -0600

    Create references for invoice and merchant

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_03_06_225810) do
+ActiveRecord::Schema[7.1].define(version: 2025_03_06_231244) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -21,6 +21,10 @@ ActiveRecord::Schema[7.1].define(version: 2025_03_06_225810) do
     t.integer "dollar_off"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "merchant_id", null: false
+    t.bigint "invoice_id", null: false
+    t.index ["invoice_id"], name: "index_coupons_on_invoice_id"
+    t.index ["merchant_id"], name: "index_coupons_on_merchant_id"
   end
 
   create_table "customers", force: :cascade do |t|
@@ -77,6 +81,8 @@ ActiveRecord::Schema[7.1].define(version: 2025_03_06_225810) do
     t.index ["invoice_id"], name: "index_transactions_on_invoice_id"
   end
 
+  add_foreign_key "coupons", "invoices"
+  add_foreign_key "coupons", "merchants"
   add_foreign_key "invoice_items", "invoices"
   add_foreign_key "invoice_items", "items"
   add_foreign_key "invoices", "customers"

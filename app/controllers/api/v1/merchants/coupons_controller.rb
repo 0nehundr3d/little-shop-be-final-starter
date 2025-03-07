9968b10ec6769736d2f9d4f3be869bd9cc81cc67
Sebastian McKee <sebmcke@gmail.com>
2025-03-07 12:23:59 -0600

    Create coupon param protection

--- app/controllers/api/v1/merchants/coupons_controller.rb
+++ app/controllers/api/v1/merchants/coupons_controller.rb
@@ -8,4 +8,14 @@ class Api::V1::Merchants::CouponsController < ApplicationController
         merchant = Merchant.find(params[:merchant_id])
         render json: CouponSerializer.new(merchant.coupons.find(params[:id]))
     end
+
+    def create 
+
+    end
+
+    private 
+
+    def coupon_params
+        params.permit(:name, :code, :percent_off, :dollar_off, :merchant_id)
+    end
 end

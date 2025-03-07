dc73fa5f1f382e863821a9d95093263b544703eb
Sebastian McKee <sebmcke@gmail.com>
2025-03-07 13:51:10 -0600

    Change coupon faker model to reflect new table layout

--- spec/factories/coupons.rb
+++ spec/factories/coupons.rb
@@ -2,8 +2,8 @@ FactoryBot.define do
     factory :coupon do
         name { Faker::Lorem.word }
         code { Faker::Lorem.unique.word }
-        dollar_off { Faker::Number.number(digits: 2) }
-        percent_off { nil }
+        percent { true }
+        discount { Faker::Number.number(digits: 2) }
         merchant
         invoice
     end

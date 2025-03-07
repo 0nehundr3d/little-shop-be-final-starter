a412b3df0131c85fcc747e650da9d6dab3233b47
Sebastian McKee <sebmcke@gmail.com>
2025-03-07 12:49:05 -0600

    Create update action and add active into allowed params

--- app/controllers/api/v1/merchants/coupons_controller.rb
+++ app/controllers/api/v1/merchants/coupons_controller.rb
@@ -14,9 +14,16 @@ class Api::V1::Merchants::CouponsController < ApplicationController
         render json: CouponSerializer.new(coupon)
     end
 
+    def update
+        coupon = Coupon.find(params[:id])
+        coupon.update!(coupon_params)
+
+        render json: CouponSerializer.new(coupon)
+    end
+
     private 
 
     def coupon_params
-        params.permit(:name, :code, :percent_off, :dollar_off, :merchant_id)
+        params.permit(:name, :code, :percent_off, :dollar_off, :merchant_id, :active)
     end
 end

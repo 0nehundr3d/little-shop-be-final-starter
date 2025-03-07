b1752fbbbce47c4459c8a476b5eb773168be7ee7
Sebastian McKee <sebmcke@gmail.com>
2025-03-07 12:11:15 -0600

    Fix show test

--- spec/requests/api/v1/merchant/coupons_request_spec.rb
+++ spec/requests/api/v1/merchant/coupons_request_spec.rb
@@ -1,7 +1,7 @@
 require "rails_helper"
 
 describe "Coupons endpoints", :type => :request do
-    describe "GET coupons" do
+    describe "GET all coupons" do
         it "should return a list of coupons" do
             merchant = create(:merchant)
             create_list(:coupon, 3, merchant: merchant)
@@ -15,16 +15,19 @@ describe "Coupons endpoints", :type => :request do
             expect(json[:data].first[:attributes]).to include(:name, :code, :dollar_off, :percent_off, :merchant_id)
         end
 
+    end
+    
+    describe "GET one coupon" do
         it "should return info about a specific coupon" do
             merchant = create(:merchant)
             coupon = create(:coupon, merchant: merchant)
-
+    
             get "/api/v1/merchants/#{merchant.id}/coupons/#{coupon.id}"
             json = JSON.parse(response.body, symbolize_names: true)
-
-            expect(response).ti have_http_status(:ok)
-            expect(json[:data].first).to include(:id, :type, :attributes)
-            expect(json[:data].first[:attributes]).to include(:name, :code, :dollar_off, :percent_off, :merchant_id)
-        end
+    
+            expect(response).to have_http_status(:ok)
+            expect(json[:data]).to include(:id, :type, :attributes)
+            expect(json[:data][:attributes]).to include(:name, :code, :dollar_off, :percent_off, :merchant_id)
+        end 
     end
 end

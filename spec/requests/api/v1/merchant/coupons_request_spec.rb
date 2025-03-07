fe512471f8d21bbf6d9287c7db1803dc6a9e78c3
Sebastian McKee <sebmcke@gmail.com>
2025-03-07 12:08:07 -0600

    Create tet for coupons show

--- spec/requests/api/v1/merchant/coupons_request_spec.rb
+++ spec/requests/api/v1/merchant/coupons_request_spec.rb
@@ -1,7 +1,7 @@
 require "rails_helper"
 
 describe "Coupons endpoints", :type => :request do
-    describe "GET all coupons" do
+    describe "GET coupons" do
         it "should return a list of coupons" do
             merchant = create(:merchant)
             create_list(:coupon, 3, merchant: merchant)
@@ -11,6 +11,18 @@ describe "Coupons endpoints", :type => :request do
 
             expect(response).to have_http_status(:ok)
             expect(json[:data].count).to eq(3)
+            expect(json[:data].first).to include(:id, :type, :attributes)
+            expect(json[:data].first[:attributes]).to include(:name, :code, :dollar_off, :percent_off, :merchant_id)
+        end
+
+        it "should return info about a specific coupon" do
+            merchant = create(:merchant)
+            coupon = create(:coupon, merchant: merchant)
+
+            get "/api/v1/merchants/#{merchant.id}/coupons/#{coupon.id}"
+            json = JSON.parse(response.body, symbolize_names: true)
+
+            expect(response).ti have_http_status(:ok)
             expect(json[:data].first).to include(:id, :type, :attributes)
             expect(json[:data].first[:attributes]).to include(:name, :code, :dollar_off, :percent_off, :merchant_id)
         end

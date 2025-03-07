a23fb312e3202d8bb0405a80eff738693dfe4a57
Sebastian McKee <sebmcke@gmail.com>
2025-03-07 12:11:29 -0600

    Add show action to coupons controller

--- app/controllers/api/v1/merchants/coupons_controller.rb
+++ app/controllers/api/v1/merchants/coupons_controller.rb
@@ -3,4 +3,9 @@ class Api::V1::Merchants::CouponsController < ApplicationController
         merchant = Merchant.find(params[:merchant_id])
         render json: CouponSerializer.new(merchant.coupons)
     end
+
+    def show
+        merchant = Merchant.find(params[:merchant_id])
+        render json: CouponSerializer.new(merchant.coupons.find(params[:id]))
+    end
 end
